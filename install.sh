29e41d5cb9a9b55d5f8bd8abfacb6e13a619b601
Michael Wood <michael.g.wood@intel.com>
2016-03-29 19:42:00 +0100

    Add python-flake8

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ mv ~/.vim ~/.vim-backup
 
 cp -r ./ ~/.vim/
 
-sudo apt-get install ruby-dev build-essential cmake python-dev
+sudo apt-get install ruby-dev build-essential cmake python-dev python-flake8
 
 vim -c PlugInstall
